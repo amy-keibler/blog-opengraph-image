35eab17ef36d07813f701eb95ea1b49907f2ecbf
Amy Keibler <amelia.keibler@gmail.com>
2021-09-06 22:46:03 -0400

    Add Nix Flake for Rust Development
    Use the naersk Rust flake approach to build this crate.
    Vendor the DejaVu font and its license

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nmattia/naersk";
+  };
+
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages."${system}";
+      naersk-lib = naersk.lib."${system}";
+    in rec {
+      # `nix build`
+      packages.blog-opengraph-image = naersk-lib.buildPackage {
+        pname = "blog-opengraph-image";
+        root = ./.;
+      };
+      defaultPackage = packages.blog-opengraph-image;
+
+      # `nix run`
+      apps.blog-opengraph-image = utils.lib.mkApp {
+        drv = packages.blog-opengraph-image;
+        exePath = "/bin/blog-image";
+      };
+      defaultApp = apps.blog-opengraph-image;
+
+      # `nix develop`
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [ rustc cargo rust-analyzer ];
+
+        RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+      };
+    });
+}
